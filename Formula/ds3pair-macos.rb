b7200c9441cd20a6cd438023c96998f4a68dd8bb
F1dg3t <57208077+F1dg3tXD@users.noreply.github.com>
2026-08-08 20:00:40 +0600

    Prepare v0.0.4a Homebrew distribution

--- Formula/ds3pair-macos.rb
+++ Formula/ds3pair-macos.rb
@@ -1,7 +1,7 @@
 class Ds3pairMacos < Formula
   desc "DualShock 3 pairing and diagnostic utility for macOS"
   homepage "https://github.com/F1dg3tXD/ds3pair-macos"
-  url "https://github.com/F1dg3tXD/ds3pair-macos/archive/refs/tags/v0.0.3a.tar.gz"
+  url "https://github.com/F1dg3tXD/ds3pair-macos/archive/refs/tags/v0.0.4a.tar.gz"
   sha256 "f2426d3b806f56587b23b9f4ab6fb3cb57acb34c2c1c730968f776984c9d6a6e"
   license "MIT"
 
